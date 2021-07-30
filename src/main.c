4c4fcf656ebbf652df5b2ca15360a3401d39970b
alek3y <alek3y@protonmail.com>
2021-07-30 19:11:17 +0200

    💫 Add eating animation

--- src/main.c
+++ src/main.c
@@ -10,6 +10,7 @@
 
 #define SNAKE_HEAD '0'
 #define SNAKE_BODY 'o'
+#define SNAKE_BODY_EAT 'O'
 #define APPLE '+'
 #define APPLE_VALUE 1
 
@@ -30,10 +31,13 @@ int main() {
 	getmaxyx(stdscr, height, width);
 
 	Board board = board_new(point_new(1, 1), point_new(width-2, height-2));
+
 	Snake player = snake_new(point_new(width/2, height/2), SNAKE_HEAD, SNAKE_BODY);
 	Point direction = point_new(-1, 0);
+
 	Apple apple = apple_new(APPLE, APPLE_VALUE);
 	apple_place(&apple, board, player);
+	size_t apple_eaten = 0;
 
 	bool run = true;
 	while (run) {
@@ -79,6 +83,41 @@ int main() {
 		}
 
 		snake_move_relative(player, board, direction);
+
+		if (point_equals(player.head->position, apple.position)) {
+			apple_eaten++;		// Start animation
+		}
+
+		if (apple_eaten > 0) {
+			struct Body *body = player.head->next;		// Head shouldn't change
+			bool is_body_eating;
+			while (body != NULL) {
+				is_body_eating = point_equals(body->position, apple.position);
+
+				if (!body->hidden) {
+					if (is_body_eating) {
+						body->symbol = SNAKE_BODY_EAT;
+					} else {
+						body->symbol = SNAKE_BODY;
+					}
+				}
+
+				body = body->next;
+			}
+
+			// Add a new body part when it reached the tail
+			if (is_body_eating) {
+
+				// TODO: Add apple to the snake as soon as it gets eaten on the head (and
+				// check if the animation works with multiple apples)
+				apple_place(&apple, board, player);
+				apple_eaten--;
+
+				for (size_t i = 0; i < APPLE_VALUE; i++) {
+					body_tail_show(player.head, SNAKE_BODY);
+				}
+			}
+		}
 	}
 
 	snake_destroy(player);
